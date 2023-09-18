c0d9efba1787ca92d4c305b27b5dcb3809f83629
imVituZeron <vitorpaulasantos@gmail.com>
2023-09-18 15:29:01 -0300

    I create a variable that take secret number and I changed style of the 'Welcome to the Our Game'

--- program.c
+++ program.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void main() {
-    printf("Welcome our game");
+    printf("***************************\n");
+    printf("* Welcome to the Our Game *\n");
+    printf("***************************\n");
+
+    int secret_number = 42;
+    
+    printf("The number %d is secret. Don't tell it to nobody!\n", secret_number);
 }
