bdee5a4751003d5faa7cb3b944a14eb5e1e75738
Vitor de Paula Santos <vitu@pop-os.localdomain>
2023-09-25 13:02:42 -0300

    I created a FOR and I placed the If conditions into it;

--- program.c
+++ program.c
@@ -7,21 +7,25 @@ void main() {
 
     int secret_number = 42;
     int kick;
+    for (int i=1; i<=3; i++) {
+        printf("Attempt %d of 3\n", i);
+        printf("Choose a number, please: ");
+        scanf("%d", &kick);
+        printf("the number chosen was %d\n", kick);    
+        
+        int right = (kick == secret_number);
 
-    printf("Choose a number, please: ");
-    scanf("%d", &kick);
-    printf("the number chosen was %d\n", kick);    
-    
-    int right = (kick == secret_number);
-
-    if (right) {
-        printf("Congratulations, You Right!!!\n");
-    } else {
-        int bigger = (kick > secret_number);
-        if (bigger) {
-            printf("Your number is bigger than secret number");
+        if (right) {
+            printf("Congratulations, You Right!!!\n");
+            break;
         } else {
-            printf("Your number is smaller than secret number");
+            int bigger = (kick > secret_number);
+            if (bigger) {
+                printf("Your number is bigger than secret number\n");
+            } else {
+                printf("Your number is smaller than secret number\n");
+            }
         }
+        printf("Game Over");
     }
 }
