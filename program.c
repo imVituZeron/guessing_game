fe70fc8905dffc57a3357f230127df51ae37b008
Vitor de Paula Santos <vitu@pop-os.localdomain>
2023-09-26 11:11:39 -0300

    I refactoring code i created a constant to result and i did changes if conditions

--- program.c
+++ program.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
+//constant
+#define FINAL_NUMBER 5
+
 void main() {
     printf("***************************\n");
     printf("* Welcome to the Our Game *\n");
     printf("***************************\n");
 
-    int secret_number = 42;
     int kick;
-    for (int i=1; i<=3; i++) {
-        printf("Attempt %d of 3\n", i);
+    int secret_number = 42;
+
+    for (int i=1; i<=FINAL_NUMBER; i++) {
+        printf("Attempt %d of %d\n", i, FINAL_NUMBER);
         printf("Choose a number, please: ");
         scanf("%d", &kick);
         printf("the number chosen was %d\n", kick);    
         
         int right = (kick == secret_number);
+        int bigger = (kick > secret_number);
+        int smaller = (kick < secret_number);
 
         if (right) {
             printf("Congratulations, You Right!!!\n");
             break;
-        } else {
-            int bigger = (kick > secret_number);
-            if (bigger) {
-                printf("Your number is bigger than secret number\n");
-            } else {
-                printf("Your number is smaller than secret number\n");
-            }
         }
-        printf("Game Over");
+        
+        if (bigger) {
+            printf("Your number is bigger than secret number\n");
+        }
+
+        if (smaller) {
+            printf("Your number is smaller than secret number\n");
+        }
     }
+    printf("Game Over\n");
 }
